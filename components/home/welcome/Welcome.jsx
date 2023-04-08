d9d4ea84a95a345c4ade1d10c99aa9f937b3a100
Kevin Mars <kevin.eth.420@gmail.com>
2023-04-09 02:39:17 +0800

    mess around

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
+
+import styles from './welcome.style'
+
+const Welcome = () => {
+  const router = useRouter()
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Kevin</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}/>
+
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
